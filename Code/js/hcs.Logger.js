7cb70b3424ac565439f2cd410ceb828c05c1f38e
吴疆 <wujiang007@gmail.com>
2014-11-03 19:59:03 +0800

    add Logger test script

--- Code/js/hcs.Logger.js
+++ Code/js/hcs.Logger.js
@@ -13,31 +13,31 @@
 var hcs = window.hcs || {};
 
 hcs.Logger = function(){
-    var __debugMode = false;
+    var _debugMode = false;
     return{
         message : function(msg) {
-            __debugMode && console.log(msg);
+            _debugMode && console.log(msg);
         }, 
         warning : function(msg) {
-            __debugMode && console.warn(msg);
+            _debugMode && console.warn(msg);
         }, 
         error : function(msg) {
-            __debugMode && console.error(msg);
+            _debugMode && console.error(msg);
         }, 
         out : function(msg) {
-            if (__debugMode) {
+            if (_debugMode) {
                 var obj = document.getElementById("debugArea");
                 obj.innerHTML = "<div>" + msg + "</div>" + obj.innerHTML;
             }
         }, 
         clear : function() {
-            if (__debugMode) {
+            if (_debugMode) {
                 var msg = document.getElementById("debugArea");
                 msg.innerHTML = "";
             }
         },
         setDebugMode : function(debugMode) {
-            __debugMode = debugMode ? true : false;
+            _debugMode = debugMode ? true : false;
         }
     };
 }();
