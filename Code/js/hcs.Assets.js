1469dd296f06309e9a4341ad686a86a36c5a231d
吴疆 <wujiang007@gmail.com>
2014-11-03 15:29:49 +0800

    add Assets

--- Code/js/hcs.Assets.js
+++ Code/js/hcs.Assets.js
@@ -2,3 +2,115 @@
  * 该模块负责存储和定义所有系统需要用到的静态资源
  */
 var hcs = window.hcs || {};
+
+hcs.Assets = {
+    globalPath: "",
+    metalTextures: {
+        diffuse: "textures/metal/metal_DIFFUSE.png",
+        normal: "textures/metal/metal_NORMAL.png",
+        specular: "textures/metal/metal_SPECULAR.png"
+    },
+    woodTextures: {
+        diffuse: "textures/bois/bois_DIFFUSE.png",
+        normal: "textures/bois/bois_NORMAL.png",
+        specular: "textures/bois/bois_SPECULAR.png"
+    },
+    plasticTextures: {
+        diffuse: "textures/plastique/plastique_DIFFUSE.png",
+        normal: "textures/plastique/plastique_NORMAL.png",
+        specular: "textures/plastique/plastique_SPECULAR.png"},
+        whiteTexture: "textures/misc/white.jpg",
+        blackTexture: "textures/misc/black.jpg",
+        transparentTexture: "textures/misc/transparent.png",
+        shadowTexture: "textures/shadow/shadow.png",
+    roomTextures: {
+        diffuse: "textures/parquet/parquet_DIFFUSE.png",
+        normal: "textures/parquet/parquet_NORMAL.png",
+        specular: "textures/parquet/parquet_SPEC.png"
+    },
+    firePlaceFire: "textures/fireplace/fireplace_tex.png",
+    firePlaceBrick: {
+        diffuse: "textures/fireplace/fire_place_brick.png",
+        normal: "textures/fireplace/fire_place_brick_normals.png",
+        specular: "textures/fireplace/fire_place_brick_spec.png"
+    },
+    firePlaceBrick2: {
+        diffuse: "textures/textureset/brick/brick05.jpg",
+        normal: "textures/textureset/brick/brick05_NORM.jpg",
+        specular: "textures/textureset/brick/brick05_SPEC.jpg"
+    },
+    firePlacePlate: {
+        diffuse: "textures/fireplace/fireplace_plate_tex.png",
+        normal: "textures/fireplace/fireplace_plate_tex_normals.png",
+        specular: "textures/fireplace/fireplace_plate_tex_spec.png"
+    },
+    induction: {
+        map: "textures/misc/induction.jpg"
+    },
+    dishwasher: {
+        map: "textures/misc/lavevaisselle.jpg"
+    },
+    hood: {
+        map: "textures/misc/hotte.png"
+    },
+    oven: {
+        map: "textures/misc/oven.png"
+    },
+    microwave: {
+        map: "textures/misc/microwave.png"
+    },
+    firePlaceWood: {
+        diffuse: "textures/fireplace/fireplace_wood.png",
+        normal: "textures/fireplace/fireplace_wood_normals.png",
+        specular: "textures/fireplace/fireplace_wood_spec.png"
+    },
+    envMapTexture: "textures/environment/SwedishRoyalCastle/src",
+    flatEnvMapTexture: "textures/environment/flatInterior/flat",
+    skyEnvMapTexture: "textures/environment/sky.jpg",
+    toolbarTextures: {
+        infoTexture: "textures/toolbar/info.png",
+        infoTextureGroup: "textures/toolbar/infoGroup.png",
+        rotateTexture: "textures/toolbar/rotate.png",
+        arrowTexture: "textures/toolbar/arrow.png"
+    },
+    mattTexture: "textures/misc/CNPflat_tex.png",
+    darkWoodTextures: {
+        diffuse: "textures/wood/dark-wood1.png",
+        normal: "textures/wood/dark-wood1_NORM.png",
+        specular: "textures/wood/dark-wood1_SPEC.png"
+    },
+    bedSheetsTextures: {
+        diffuse: "textures/bed/sheets.png",
+        normal: "textures/bed/sheets_NORM.png",
+        specular: "textures/bed/sheets_SPEC.png"
+    },
+    cromwellTextures: {
+        diffuse: "models/babylon/chair/cromwell/cromwell_diff.png",
+        normal: "models/babylon/chair/cromwell/cromwell_normals.png",
+        specular: "models/babylon/chair/cromwell/cromwell_specular.png"
+    },
+    cushionTexture: "textures/bed/cushion-test3.png",
+    vasqueDAE: "models/babylon/bathroom/components/vasque_s",
+    doucheDAE: "models/babylon/bathroom/shower1/shower1.babylon",
+    robinetDAE: "models/babylon/robinet/robinet.babylon",
+    spotLampDAE: "models/babylon/lamps/lamp2/spot.babylon",
+    robinet3DAE: "models/babylon/robinet/robinet3.babylon",
+    robinet4DAE: "models/babylon/robinet/robinet4.babylon",
+    robinet5DAE: "models/babylon/robinet/tap.babylon",
+    boconceptfeetDAE: "models/babylon/table/boconcept/bocpart.babylon",
+    ovaletableDAE: "models/babylon/table/ovale/ovale.babylon",
+    cromwellleftsideDAE: "models/babylon/chair/cromwell/cromwell_leftside.babylon",
+    cromwelrightsideDAE: "models/babylon/chair/cromwell/cromwell_rightside.babylon",
+    cromwellcenterDAE: "models/babylon/chair/cromwell/cromwell_center.babylon",
+    DaePath: "models/babylon/",
+    retrieveImageScriptPath: "php/retrieveImage.php",
+    retrieveObjectScriptPath: "php/retrieveObject.php",
+    remote: {
+        man: "images/remote-controller/green_man.png",
+        cam: "images/remote-controller/green_cam.png",
+        bg: "images/remote-controller/bg.png"
+    },
+    mainUIColor: "#8AB808",
+    complementaryUIColor: "#BDB808",
+    groupColor: "#B88A08"
+};
