d49c62c1e1e32c512fc7fefe31991de9f4f83030
kei0502 <kei0502@qq.com>
2014-12-10 13:32:01 +0800

    添加问卷调查按钮 修改css

--- HCS/js/GridComponent2D.js
+++ HCS/js/GridComponent2D.js
@@ -22,15 +22,15 @@ var GridComponent2D = function() {
                 p = this.core.getHeight() / 2 + (this.core.engine2D.getZoom() * Math.abs(c.min.y) - this.core.getHeight() / 2),
                 d = this.core.getWidth() / 2 - (this.core.engine2D.getZoom() * Math.abs(c.max.x) - this.core.getWidth() / 2),
                 m = this.core.getHeight() / 2 - (this.core.engine2D.getZoom() * Math.abs(c.max.y) - this.core.getHeight() / 2);
-            s = e.x >= u ? e.x - u : s, l = e.x >= u ? s : 0, a = e.y >= p ? e.y - p : a, h = e.y >= p ? a : 0, i = e.x < d ? i + e.x - d : i, o = e.y < m ? o + e.y - m : o, t.strokeStyle = "#dddddd", t.lineWidth = 1, t.beginPath();
+            s = e.x >= u ? e.x - u : s, l = e.x >= u ? s : 0, a = e.y >= p ? e.y - p : a, h = e.y >= p ? a : 0, i = e.x < d ? i + e.x - d : i, o = e.y < m ? o + e.y - m : o, t.strokeStyle = "#aaa", t.lineWidth = 1, t.beginPath();
             for (var g = a; o > g; g += r)
                 t.moveTo(l, Math.floor(g) + .5), t.lineTo(i, Math.floor(g) + .5);
             for (var f = s; i > f; f += r)
                 t.moveTo(Math.floor(f) + .5, h), t.lineTo(Math.floor(f) + .5, o);
-            t.stroke(), t.strokeStyle = "#aaaaaa", t.lineWidth = 3, t.beginPath(), e.x >= u && (t.moveTo(Math.floor(s) + .5, h), t.lineTo(Math.floor(s) + .5, o)), e.x <= d && (t.moveTo(Math.floor(i) + .5, h), t.lineTo(Math.floor(i) + .5, o)), e.y >= p && (t.moveTo(l, Math.floor(a) + .5), t.lineTo(i, Math.floor(a) + .5)), e.y <= m && (t.moveTo(l, Math.floor(o) + .5), t.lineTo(i, Math.floor(o) + .5)), t.stroke(), t.fillStyle = "#770000", t.fillRect(e.x - 5, e.y - 5, 11, 11);
+            t.stroke(), t.strokeStyle = "#f0f0f0", t.lineWidth = 3, t.beginPath(), e.x >= u && (t.moveTo(Math.floor(s) + .5, h), t.lineTo(Math.floor(s) + .5, o)), e.x <= d && (t.moveTo(Math.floor(i) + .5, h), t.lineTo(Math.floor(i) + .5, o)), e.y >= p && (t.moveTo(l, Math.floor(a) + .5), t.lineTo(i, Math.floor(a) + .5)), e.y <= m && (t.moveTo(l, Math.floor(o) + .5), t.lineTo(i, Math.floor(o) + .5)), t.stroke(), t.fillStyle = "#770000", t.fillRect(e.x - 5, e.y - 5, 11, 11);
             var y = 100,
                 _ = "1m";
-            n > 2.5 && (y = 50, _ = "0.5m"), t.strokeStyle = "#999999", t.beginPath(), t.lineWidth = 3, t.moveTo(20, o - 30.5), t.lineTo(Math.floor(20 + y * n), o - 30.5), t.stroke(), t.lineWidth = 2, t.moveTo(20, o - 29), t.lineTo(20, o - 35), t.moveTo(Math.floor(20 + y * n), o - 29), t.lineTo(Math.floor(20 + y * n), o - 35), t.stroke(), t.fillStyle = "#777777", t.font = "10px Verdana", t.textAlign = "center", t.fillText(_, y / 2 * n + 20, o - 15)
+            n > 2.5 && (y = 50, _ = "0.5m"), t.strokeStyle = "#999", t.beginPath(), t.lineWidth = 3, t.moveTo(20, o - 30.5), t.lineTo(Math.floor(20 + y * n), o - 30.5), t.stroke(), t.lineWidth = 2, t.moveTo(20, o - 29), t.lineTo(20, o - 35), t.moveTo(Math.floor(20 + y * n), o - 29), t.lineTo(Math.floor(20 + y * n), o - 35), t.stroke(), t.fillStyle = "#999", t.font = "10px Verdana", t.textAlign = "center", t.fillText(_, y / 2 * n + 20, o - 15)
         }
     }, t.prototype.onDragStart = function() {
         this.core.engine2D.registerEventCb("GridComponent2D.dragging", this.priority, "dragging", this.core.engine2D.MODE_DRAG, null, this.onDragging.bind(this), null), this.core.engine2D.registerEventCb("GridComponent2D.drag-end", this.priority, "drag-end", this.core.engine2D.MODE_DRAG, null, this.onDragEnd.bind(this), null)
