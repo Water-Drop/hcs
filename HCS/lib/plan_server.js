cbebd6765e096c0d265e51a7896ada3d044ca485
bandaot <wujiang007@gmail.com>
2014-12-17 18:52:14 +0800

    修正reconnect数据不存在的bug

--- HCS/lib/plan_server.js
+++ HCS/lib/plan_server.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
-var plans = {};
 var users = {};
+var plans = {};
 
 exports.start = function (app) {
     app.io.set('log level', 1);
@@ -19,6 +19,33 @@ exports.saveToDisk = function (){
     }
     console.log('Save data completed.');
 }
+function loadPlan(uuid, exec) { 
+    if (plans[uuid]) {
+        console.log('read data from mem!');
+        exec();
+    } else {
+        var path = getStorePath(uuid);
+        fs.exists(path, function (exists) {
+            //Check if file exists
+            if (exists) {
+                fs.readFile(path, function (err, data) {
+                    try {
+                        plans[uuid] = JSON.parse(data);
+                    }
+                    catch (e) {
+                        plans[uuid] = {};
+                    }
+                    exec();
+                });
+            } else {
+                plans[uuid] = {};
+                exec();
+            }
+                
+        });
+    }
+}
+
 function getStorePath(uuid) { 
     return 'data/plans/' + uuid + '.json';
 }
@@ -27,30 +54,9 @@ function handleInit(app) {
         console.log(req.data.uuid + ' join');
         var uuid = req.data.uuid;
         users[req.socket.id] = uuid;
-        if (plans[uuid]) {
-            console.warn('read data from mem!');
+        loadPlan(uuid, function () { 
             req.io.respond(plans[uuid]);
-        } else {
-            var path = getStorePath(uuid);
-            fs.exists(path, function (exists) {
-                //Check if file exists
-                if (exists) {
-                    fs.readFile(path, function (err, data) {
-                        try {
-                            plans[uuid] = JSON.parse(data);
-                        }
-                        catch (e) {
-                            plans[uuid] = {};
-                        }
-                        req.io.respond(plans[uuid]);
-                    });
-                } else {
-                    plans[uuid] = {};
-                    req.io.respond({});
-                }
-                
-            });
-        }
+        });
     });
 }
 
@@ -58,16 +64,23 @@ function handleStorage(app) {
     app.io.route('get', function (req) {
         var uuid = users[req.socket.id];
         console.log(plans[uuid]);
-        data = plans[uuid][req.data.key] || null;
-        req.io.respond(data);
+        loadPlan(uuid, function (plan) {
+            data = plans[uuid][req.data.key] || null;
+            req.io.respond(data);
+        });
     });
     app.io.route('set', function (req) {
         var uuid = users[req.socket.id];
-        plans[uuid][req.data.key] = req.data.val;
+        loadPlan(uuid, function (plan) {
+            plans[uuid][req.data.key] = req.data.val;
+        });
+        
     });
-    app.io.route('del', function (req) { 
+    app.io.route('del', function (req) {
         var uuid = users[req.socket.id];
-        delete plans[uuid][req.data.key];
+        loadPlan(uuid, function (plan) {
+            delete plans[uuid][req.data.key];
+        });        
     });
 }
 
